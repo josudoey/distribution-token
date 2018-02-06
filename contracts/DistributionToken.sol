48dbe6ce28662f390ad57af5892e3760e71bd90c
Joey Fang <josudoey@gmail.com>
2018-02-06 17:58:47 +0800

    [why] patch for example

--- contracts/DistributionToken.sol
+++ contracts/DistributionToken.sol
@@ -299,7 +299,7 @@ contract SimpleToken is DistributionToken {
   uint256 public decimals = 18;
 }
 
-contract InitialCoinOffering is Ownable{
+contract InitialCoinOffering is Ownable {
   DistributionToken public token;
   address issuer;
   uint256 initialSupply;
@@ -318,3 +318,44 @@ contract InitialCoinOffering is Ownable{
   }
 }
 
+contract CrowdSale is Ownable {
+  using SafeMath for uint256;
+
+  DistributionToken public token;
+
+  event Sale(address indexed customer, uint256 value, uint256 amount);
+
+  uint256 public totalSales;
+
+  // address where funds are collected
+  address public wallet;
+
+  // how many token units a buyer gets per wei
+  uint256 public rate;
+
+  uint256 public weiRaised;
+
+  function CrowdSale(DistributionToken _token,  uint256 _rate) public {
+    require(_token != address(0));
+    require(_rate != 0);
+    token = _token;
+    rate = _rate;
+    wallet = msg.sender;
+  }
+
+  function () public payable {
+    uint256 amount = rate.mul(msg.value);
+    totalSales = totalSales.add(amount);
+
+    weiRaised = weiRaised.add(msg.value);
+
+    token.transfer(msg.sender, amount);
+    wallet.transfer(msg.value);
+    Sale(msg.sender, msg.value, amount);
+  }
+
+  function remaining() public view returns (uint256) {
+    return token.balanceOf(this);
+  }
+
+}
--- test/base.js
+++ test/base.js
@@ -1,5 +1,6 @@
 const Token = artifacts.require('SimpleToken')
 const ICO = artifacts.require('InitialCoinOffering')
+const CrowdSale = artifacts.require('CrowdSale')
 const assert = require('assert')
 const Eth = require('ethjs')
 const eth = new Eth(web3.currentProvider)
@@ -7,9 +8,11 @@ const eth = new Eth(web3.currentProvider)
 contract('InitialCoinOffering', function (accounts) {
   let token
   let ico
+  let crowdsale
   const owner = web3.eth.accounts[0]
   const issuer = web3.eth.accounts[1]
-  const initailSupply = 10
+  const guest = web3.eth.accounts[2]
+  const initailSupply = 10000
 
   before(async function () {
     token = await Token.new()
@@ -22,5 +25,31 @@ contract('InitialCoinOffering', function (accounts) {
     const balance = await token.balanceOf(issuer)
     assert(balance.toString() === initailSupply.toString())
   })
+
+  it('crowdsale case', async function () {
+    const rate = 10
+    crowdsale = await CrowdSale.new(token.address, rate)
+    await token.transfer(crowdsale.address, 1000, {
+      from: issuer
+    })
+    const balance = await token.balanceOf(issuer)
+    assert(balance.toString() === '9000')
+
+    const tx = await eth.sendTransaction({
+      from: guest,
+      to: crowdsale.address,
+      value: 20,
+      gas: 3000000,
+      data: '0x'
+    })
+
+    let remaining = await crowdsale.remaining()
+    assert(remaining.toString() === '800')
+
+    const guestBalance = await token.balanceOf(guest)
+    assert(guestBalance.toString() === '200')
+
+  })
+
 })
 
